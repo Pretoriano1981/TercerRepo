0cc72758ca237c5b237e39b02e9e5e9cb68a30da
pretoriano <marcas1981@gmail.com>
2026-02-23 11:15:24 -0500

    Primer Lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                                      # Nombre del paquete
+    version="0.1.0",                                        # Versión inicial
+    packages=find_packages(),                               # Paquetes a incluir
+    description="Un paquete pip simple de saludo",          # Breve descripción
+    author="Mario",                                         # Tu nombre
+    author_email="marcas1981@gmail.com",                    # Tu correo electrónico
+    url="https://github.com/Pretoriano1981/TercerRepo",     # URL del proyecto
+)
